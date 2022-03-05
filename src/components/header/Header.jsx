6b8ae84f16e4d2c0914e3724b2d07f15647aacb1
Naveed-Sarwar <naveedlefti11@gmail.com>
2022-03-05 12:15:27 +0500

    Work

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavMenu,
+  MobileIcon,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink
+} from "./HeaderStyled";
+import { FaBars } from "react-icons/fa";
+const Header = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">dolla</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+              <NavLinks to="discover">Discover</NavLinks>
+              <NavLinks to="services">Services</NavLinks>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+             <NavBtnLink to ="/signin">Sign in</NavBtnLink>
+          </NavBtn> 
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Header;
