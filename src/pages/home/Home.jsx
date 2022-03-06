843026632573bb667aa066d28dc807bc7af0f9f6
Naveed-Sarwar <naveedlefti11@gmail.com>
2022-03-06 20:04:33 +0500

    Work

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <HomeContainer>
+        <HomeBackground>
+            <Video autoplay loop muted src={Video} type='video/mp4'/>
+        </HomeBackground>
+    </HomeContainer>
   )
 }
 
-export default Home
+export default Home;
---
+++ src/pages/home/HomeStyled.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+background: #0c0c0c;
+display: fllex;
+justify-content: center;
+align-items: center;
+padding: 0 30px;
+height: 800px;
+position: relative;
+z-index: 1
+`
+export const HomeBackground= styled.div`
+position: absolute;
+top: 0;
+right: 0;
+bottom: 0;
+left: 0;`
