ea8c407ab9b5b8159639e5546b960fd32aefa47e
Naveed-Sarwar <naveedlefti11@gmail.com>
2022-03-12 16:55:47 +0500

    working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 
 import './App.css';
-import Header from './components/header/Header';
 import SignIn from './pages/signin/SignIn';
 import Routing from './routing/Routing';
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <div>
       {/* <Routing /> */}
       <Router>
-        <Header />
         <Routes>
           <Route exact path="/" element={<Routing />} />
           <Route path="/signin" element={<SignIn />} />
